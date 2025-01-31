6a5778797a39e816a511044e132a80d963068178
unknown <guideoliveira2197@gmail.com>
2025-01-31 00:16:02 -0300

    curriculo atualizado

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
     
     
-    link.href = 'https://drive.google.com/file/d/1hweAOkZCxi4CCiRafMykWT7GS4XvVoMm/view?usp=sharing'; 
+    link.href = 'https://drive.google.com/file/d/1SLi1Nb7J4tzRmnAqADq5LfYNawewRhCj/view?usp=sharing'; 
     
     
     link.download = 'Curriculo_Guilherme.pdf';
